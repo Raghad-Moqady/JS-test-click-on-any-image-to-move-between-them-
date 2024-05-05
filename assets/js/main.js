8e0097f5214feaf628c7b0feeb808743a8a760b4
Raghad-Moqady <raghadmoqady@gmail.com>
2024-05-05 14:50:16 +0300

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+console.log('hello');
+var mainImg = document.getElementById('mainImg');
+function change1(){
+    mainImg.setAttribute("src","assets/img/p1.png");
+}
+function change2(){
+    mainImg.setAttribute("src","assets/img/p2.png");
+}
+function change3(){
+    mainImg.setAttribute("src","assets/img/p3.png");
+}
